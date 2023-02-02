e017a07c787f01dd18c56ab9a2612bfc6b612cc3
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-02-02 12:59:40 -0300

    Aplicando Streams no módulo HTTP -2:31

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,23 +1,45 @@
-// Stream -> 
+// Stream ->
 
 // process.stdin
 //   .pipe(process.stdout)
 
-import { Readable }  from 'node:stream';
+import { Readable, Writable, Transform, Duplex } from "node:stream";
 
 class OneToHundredStream extends Readable {
-  index = 1
-  
-  _read() {
-    const i = this.index++
+  index = 1;
 
-    if(i > 100) {
-      this.push(null)
-    } else {
-      this.push(i)
-    }
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i));
+
+        this.push(buf);
+      }
+    }, 1000);
+  }
+};
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback){
+    console.log(Number(chunk.toString()) * 10);
+    callback();
   }
 }
 
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+// new OneToHundredStream().pipe(process.stdout);
+// new OneToHundredStream().pipe(new MultiplyByTenStream());
 new OneToHundredStream()
-  .pipe(process.stdout)
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());
